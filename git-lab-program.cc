e79175df4e901e8f2905a3b9c7bc36f8024f1d9b
BP-2 <57198128+BP-2@users.noreply.github.com>
2021-09-03 17:24:40 -0400

    Add files via upload

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Brady Phelps
+ *        Date: 09/03/21
+ * Description: A program :)
+ */
+
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
